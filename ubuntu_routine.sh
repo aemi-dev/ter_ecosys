49b41ef726cd34b1a873bb13486c11932088dc3d
originecode <originecode@users.noreply.github.com>
2020-03-07 16:44:27 +0100

    PUNF Update

---
+++ ubuntu_routine.sh
@@ -0,0 +1,4 @@
+sudo apt-get update && sudo apt-get upgrade;
+sudo apt-get install python python3 ipython;
+sudo apt-get install python3-pip python-pip jupyter;
+pip install --user numpy pandas ipywidgets networkx matplotlib pydotplus pydot tatsu prince colour bitarray sympy;
