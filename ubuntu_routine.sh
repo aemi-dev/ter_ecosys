8cc589792beeff0ba719edb2cf961dd4c05f4e01
originecode <originecode@users.noreply.github.com>
2020-03-07 19:00:00 +0100

    Routine fixed

--- ubuntu_routine.sh
+++ ubuntu_routine.sh
@@ -2,8 +2,8 @@ sudo apt-get update && sudo apt-get upgrade;
 sudo apt-get install python python3 ipython ipython3;
 sudo apt-get install python3-pip python-pip jupyter;
 
-pip install --user scikit-learn numpy pandas ipywidgets pathlib cython networkx matplotlib pydotplus pydot tatsu prince colour bitarray sympy;
-pip3 install --user scikit-learn numpy pandas ipywidgets pathlib cython networkx matplotlib pydotplus pydot tatsu prince colour bitarray sympy;
+pip install --user scikit-learn numpy pandas ipywidgets pathlib cython networkx matplotlib pydotplus pydot tatsu prince colour bitarray sympy bqplot psutil;
+pip3 install --user scikit-learn numpy pandas ipywidgets pathlib cython networkx matplotlib pydotplus pydot tatsu prince colour bitarray sympy bqplot psutil;
 
 python3 mkecco.py;
 python3 ecco/__init__.py;
